6f07719c28c49bef6c6199fccb6b1c05dfdf48e5
Ahmad Alawneh <aalawneh19@gmail.com>
2023-10-23 00:27:41 +0300

    hopefully this should fix images

--- src/components/imageModal/ImageModal.js
+++ src/components/imageModal/ImageModal.js
@@ -4,7 +4,7 @@ import imageModalStyles from './imageModal.module.scss';
 export default function RenderImageModal({ image, title, clickCallback }) {
 	return (
 		<div className='imageModal' onClick={() => clickCallback(image, title)}>
-			<Image src={image} width={0} height={0} sizes="100vw" fill alt={title} className={imageModalStyles.image} onClick={(e) => e.stopPropagation()}/>
+			<Image src={image} width={0} height={0} sizes='100vw' alt={title} className={imageModalStyles.image} onClick={(e) => e.stopPropagation()}/>
 			<p className={imageModalStyles.imageTitle} onClick={(e) => e.stopPropagation()}>{title}</p>
 		</div>
 	);
